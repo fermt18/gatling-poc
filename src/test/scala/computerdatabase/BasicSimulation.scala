3c82060f4977d35da947eebd98be5e82f4c3884a
hgome <hector.gomes.sge@mango.com>
2018-09-21 12:34:33 +0200

    Prueba con servicio Post para obtener listado de pedidos, el servicio no parece ejecutarse desde gatling

--- src/test/scala/computerdatabase/BasicSimulation.scala
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -7,7 +7,7 @@ import scala.concurrent.duration._
 class BasicSimulation extends Simulation {
 
   val httpConf = http
-    .baseURL("http://shop.pre.mango.com/es") // Here is the root for all relative URLs
+    .baseURL("http://shop.pre.mango.com") // Here is the root for all relative URLs
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-US,en;q=0.5")
@@ -16,14 +16,13 @@ class BasicSimulation extends Simulation {
 
   val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
     .exec(http("Home")
-      .get("/"))
-      .pause(7) // Note that Gatling has recorder real time pauses
-    .exec(http("Favorites")
-      .get("/favorites.faces"))
-      .pause(2)
-    .exec(http("Woman")
-      .get("/mujer"))
-      .pause(2)
+      .get("/es"))
+    .exec(http("Detalle_pedido")
+      .post("/services/miscompras/getDetallePedido/online/BL2V6E/es/ES/8/0/ES?customerId=10581047&token=W0JAMzdjYzliMDExNTM1OTcyMjE3NTU4")
+      .header("Content-Length", "552")
+      .asJSON
+      .check(jsonPath("$.id").exists)
+      .check(status is 200))
 
  /* .exec(http("request_10") // Here's an example of a POST request
       .post("/computers")
@@ -32,5 +31,5 @@ class BasicSimulation extends Simulation {
         .formParam("""discontinued""", """""")
         .formParam("""company""", """37"""))*/
 
-  setUp(scn.inject(atOnceUsers(100)).protocols(httpConf))
+  setUp(scn.inject(atOnceUsers(10)).protocols(httpConf)).maxDuration(50 seconds)
 }
