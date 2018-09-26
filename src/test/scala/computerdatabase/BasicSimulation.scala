df48f08011182c0687e03c7338f8b716d10eacd6
FERRAN MONTES TORNS <ferran.montes@mango.com>
2018-09-26 18:26:18 +0200

    include new features

--- src/test/scala/computerdatabase/BasicSimulation.scala
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -12,24 +12,39 @@ class BasicSimulation extends Simulation {
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
-    //.disableFollowRedirect // disable redirects that follow in case of 301, 302, 303 or 307 response
+  //.disableFollowRedirect // disable redirects that follow in case of 301, 302, 303 or 307 response
 
-  val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
-    .exec(http("Home")
-      .get("/es"))
-    .exec(http("Detalle_pedido")
+  object Browse {
+
+    val feeder = csv("country_code.csv").random
+    val browse = repeat( 2, "n") { // runs for each user
+      exec(http("Home")
+        .get("/"))
+        .pause(3)
+        .feed(feeder)
+        .exec(http("Random Country: ${code}")
+          .get("/${code}"))
+        .pause(3)
+    }
+  }
+
+  object Pedidos {
+
+    val pedidos =
+      exec(http("Pedidos")
       .post("/services/miscompras/getDetallePedido/online/BL2V6E/es/ES/8/0/ES?customerId=10581047&token=W0JAMzdjYzliMDExNTM1OTcyMjE3NTU4")
       .header("Content-Length", "552")
       .asJSON
       .check(jsonPath("$.id").exists)
       .check(status is 200))
+      .pause(3)
+  }
 
- /* .exec(http("request_10") // Here's an example of a POST request
-      .post("/computers")
-        .formParam("""name""", """Beautiful Computer""") // Note the triple double quotes: used in Scala for protecting a whole chain of characters (no need for backslash)
-        .formParam("""introduced""", """2012-05-30""")
-        .formParam("""discontinued""", """""")
-        .formParam("""company""", """37"""))*/
+  val not_logged_users = scenario("Not Looged").exec(Browse.browse)
+  val logged_users = scenario("Logged").exec(Browse.browse, Pedidos.pedidos)
 
-  setUp(scn.inject(atOnceUsers(10)).protocols(httpConf)).maxDuration(50 seconds)
+  setUp(
+    not_logged_users.inject(atOnceUsers(100)),
+    logged_users.inject(rampUsers(10).over(10 seconds))
+  ).protocols(httpConf)
 }
